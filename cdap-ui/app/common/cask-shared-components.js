6a9be8ee1581134c4bfb8e35471db6cb0f4bc8ad
Ajai <ajainarayanan@gmail.com>
2018-09-10 11:43:46 -0700

    Fixes the PipelineConfigConstants export to not export default

--- cdap-ui/app/common/cask-shared-components.js
+++ cdap-ui/app/common/cask-shared-components.js
@@ -58,7 +58,7 @@ var PipelineConfigurationsActionCreator = require('../cdap/components/PipelineCo
 var ThemeHelper = require('../cdap/services/ThemeHelper');
 var Footer = require('../cdap/components/Footer').default;
 var cdapavscwrapper = require('../cdap/services/cdapavscwrapper').default;
-var PipelineConfigConstants = require('../cdap/components/PipelineConfigurations/PipelineConfigConstants').default;
+var PipelineConfigConstants = require('../cdap/components/PipelineConfigurations/PipelineConfigConstants');
 
 export {
   Store,
